6f842344d089f8016c9bb57e09cbb6d07d98dee6
Ismael Althenofer <ismaelalthenofer85@gmail.com>
2024-11-17 20:44:07 -0300

    se crean los archivos iniciales

---
+++ Gym-FitPower/src/main/java/fitpower/GymFitPower.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fitpower;
+
+/**
+ *
+ * @author Ismael
+ */
+public class GymFitPower {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
